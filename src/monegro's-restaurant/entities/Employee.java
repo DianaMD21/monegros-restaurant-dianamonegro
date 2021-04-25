011226048705696618c5b53c3a42a376e4849c53
Diana <diana2monegro1@gmail.com>
2021-04-24 21:46:21 -0400

    Adding Employee entity

---
+++ src/monegro's-restaurant/entities/Employee.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Employee extends Person{
+    String employeeId;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+}
