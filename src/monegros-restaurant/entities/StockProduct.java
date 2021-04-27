15e6909110dd7f3af3b4d9bec03b6ac7a8d716c2
Diana <diana2monegro1@gmail.com>
2021-04-27 13:28:09 -0400

    Adding StockProduct Entity

---
+++ src/monegros-restaurant/entities/StockProduct.java
@@ -0,0 +1,56 @@
+package entities;
+
+public class StockProduct {
+    private Long id;
+    private Stock stock;
+    private Product product;
+    private Integer quantity;
+    private String measureUnit;
+
+    public StockProduct() {
+    }
+
+    public StockProduct(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Stock getStock() {
+        return stock;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasureUnit() {
+        return measureUnit;
+    }
+
+    public void setMeasureUnit(String measureUnit) {
+        this.measureUnit = measureUnit;
+    }
+}
