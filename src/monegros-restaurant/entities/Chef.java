a63675e39f0d8de3624e4443c86586f19e8ffdf1
Diana <diana2monegro1@gmail.com>
2021-04-27 13:47:17 -0400

    Adding list of Orders to Cook to Chef Entity

--- src/monegros-restaurant/entities/Chef.java
+++ src/monegros-restaurant/entities/Chef.java
@@ -1,2 +1,31 @@
-package entities;public class Chef {
+package entities;
+
+import java.util.List;
+
+public class Chef extends Employee{
+    private int experience;
+    private List<Order> ordersToCook;
+    public Chef() {
+        super();
+
+    }
+
+    public Chef(String id) {
+        super(id);
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+    public List<Order> getOrdersToCook() {
+        return ordersToCook;
+    }
+
+    public void setOrdersToCook(List<Order> ordersToCook) {
+        this.ordersToCook = ordersToCook;
+    }
 }
