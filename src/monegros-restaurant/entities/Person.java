f28143532aa872f4cd81d4521fd0dcc0f7fae0d3
Diana <diana2monegro1@gmail.com>
2021-04-27 10:49:34 -0400

    Adding Person Entity

---
+++ src/monegros-restaurant/entities/Person.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.GregorianCalendar;
+
+public class Person {
+    private String name;
+    private String id;
+    private String lastName;
+    private String phone;
+    private String address;
+    private GregorianCalendar birthdate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public GregorianCalendar getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(GregorianCalendar birthdate) {
+        this.birthdate = birthdate;
+    }
+}
