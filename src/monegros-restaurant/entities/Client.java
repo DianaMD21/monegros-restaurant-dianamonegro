30ca66841bb5f6a912d5a1089aba58d179a13bf0
Diana <diana2monegro1@gmail.com>
2021-04-27 11:41:33 -0400

    Adding Client Entity

---
+++ src/monegros-restaurant/entities/Client.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Client extends Person{
+    private String clientId;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+}
